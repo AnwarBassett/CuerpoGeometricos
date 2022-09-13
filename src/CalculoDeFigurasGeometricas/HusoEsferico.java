4829e44bdf6113e8018b48ffabafec50e6e89f56
AnwarBassett <abassettma@gmail.com>
2022-09-13 15:12:25 -0600

    Figuras geometricas

--- src/CalculoDeFigurasGeometricas/HusoEsferico.java
+++ src/CalculoDeFigurasGeometricas/HusoEsferico.java
@@ -3,19 +3,16 @@ package CalculoDeFigurasGeometricas;
 public class HusoEsferico {
 
 	public static void main(String[] args) {
-		double Area, Volumen, r, n;
+		double Area, r, n;
 		
 		r=8;
 		n=45;
 		Area= (Math.PI*Math.pow(r,2)*n)/90;
-		Volumen = (Math.PI*Math.pow(r,3)*n)/270;
 
 		System.out.println("1.Datos de la Esfera:");
-		System.out.println("2.Radio: "+r);
-		System.out.println("3.Area Total: "+Area);
-		System.out.println("4.Volumen: "+Volumen);
-
-
+		System.out.println("2.Grados: "+n);
+		System.out.println("3.Radio: "+r);
+		System.out.println("4.Area Total: "+Area);
 	}
 
 }
