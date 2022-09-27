9ff66a2047caa2754781360cf6902d5c63a5ddec
AnwarBassett <111157669+AnwarBassett@users.noreply.github.com>
2022-09-26 21:42:40 -0600

    Add files via upload

--- src/CalculoDeFigurasGeometricas/Prisma.java
+++ src/CalculoDeFigurasGeometricas/Prisma.java
@@ -1,10 +1,10 @@
 package CalculoDeFigurasGeometricas;
 import java.util.Scanner;
-public class Piramide {
+public class Prisma {
 	public static void main(String[] args) {
 		Scanner lec=new Scanner(System.in);
 		int h, n;
-		double l, At, Pb, angulo, ap, Al, Abase, Volumen, AP;
+		double l, At, Pb, angulo, ap, Al, Abase, Volumen;
 		do{System.out.println("Digite la altura");
 		h=lec.nextInt();}while(h<=0);
 		do{System.out.println("Digite el numero de lado");
@@ -12,13 +12,12 @@ public class Piramide {
 		do{System.out.println("Digite la longitud de los lados");
 		l=lec.nextDouble();}while(l<=0);
 		Pb=l*n;
+		Al=Pb*h;
 		angulo=360/(2*n);
 		ap=l/(2*Math.tan(angulo));
-		AP=Math.pow(h,2)+Math.pow(ap,2);
-		Al=(Pb*AP)/2;
-		Abase=Math.pow(l, 2);
-		At=Al+Abase;
-		Volumen=(Abase*h)/3;
+		Abase=(Pb*ap)/2;
+		At=Al+(2*Abase);
+		Volumen=Abase*h;
 		
 		System.out.println("Datos del prisma");
 		System.out.println("1.Altura:"+h);
@@ -26,10 +25,10 @@ public class Piramide {
 		System.out.println("3.Numero de lados:"+n);
 		System.out.println("4.Perimetro de la base:"+Pb);
 		System.out.println("5.Apotema:"+ap);
-		System.out.println("6.Apotema base:"+AP);
-		System.out.println("7.Area lateral:"+Al);
-		System.out.println("8.Area tota:"+At);
-		System.out.println("9.Volumen:"+Volumen);
+		System.out.println("6.Area lateral:"+Al);
+		System.out.println("7.Area tota:"+At);
+		System.out.println("8.Volumen:"+Volumen);
 		
 	}
+	
 }
